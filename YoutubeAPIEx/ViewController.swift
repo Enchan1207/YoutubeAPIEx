b6fbb8ae4b78212a72c19fb5346a529ddab933f7
Enchan <enchant0130@gmail.com>
2021-03-22 16:03:17 +0900

    [Update] store config to userDefauts

--- YoutubeAPIEx/ViewController.swift
+++ YoutubeAPIEx/ViewController.swift
@@ -11,25 +11,37 @@ import SafariServices
 
 class ViewController: UIViewController {
     
+    private let userDefaults = UserDefaults.standard
+    private var credential: YoutubeKit.AccessCredential?
+    
     private let youtube = YoutubeKit(apiCredential: YoutubeKit.APICredential(APIKey: DefaultCredential.APIKey, clientID: DefaultCredential.clientID, clientSecret: DefaultCredential.clientSecret), accessCredential: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let storedCredentialString = self.userDefaults.object(forKey: "CREDENTIAL") as? String,
+           let storedCredential = YoutubeKit.AccessCredential.deserialize(object: storedCredentialString){
+            print("stored credential has been loaded.")
+            self.credential = storedCredential
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let credential = self.credential{
+            youtube.accessCredential = credential
+            youtube.getPlayList(part: ["id", "snippet"], channelID: nil, maxResults: 50)
+        }
     }
     
     @IBAction func onTapAuth(_ sender: Any) {
         // 認証画面を開く
         let scope: [YoutubeKit.Scope] = [.readwrite, .audit]
         youtube.authorize(presentViewController: self, scope: scope) { (credential) in
-//            print(credential)
-            print(credential.serialize()!)
-            print(YoutubeKit.AccessCredential.deserialize(object: credential.serialize()!))
-//            self.youtube.getPlayList(part: ["id", "snippet"], channelID: nil)
+            // credentialを保存
+            print("success! credential will be saved.")
+            self.credential = credential
+            self.userDefaults.setValue(credential.serialize()!, forKey: "CREDENTIAL")
         } failure: { (error) in
             print(error)
         }
-
     }
-    
 }
